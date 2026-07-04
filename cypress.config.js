c6a60d488a1ec273b3941026bfe0f15c6ac0a7f2
Bruno Gamarra <brunisoyt@gmail.com>
2026-07-03 19:21:46 -0500

    changes

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://labs15c-kum941qfh-ados-projects-0eb7f124.vercel.app/',
+    baseUrl: 'http://localhost:3000/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
